bd25ed50407e275627968ee79dc9bcdbed68b543
Pieter <pieterbrandsen2@gmail.com>
2021-02-15 21:07:36 +0100

    Added memory initialization methods

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import ErrorMapper from "./utils/errorMapper";
+import Initialization from "./memory/initialization";
 
 /**
  * Run the complete bot without ErrorMapper
  */
-function unwrappedLoop(): void {}
+function unwrappedLoop(): void {
+  if (!Initialization.IsGlobalMemoryInitialized())
+    Initialization.InitializeGlobalMemory();
+}
 
 const loop = ErrorMapper.wrapLoop(unwrappedLoop);
 
--- src/memory/initialization.ts
+++ src/memory/initialization.ts
@@ -1,7 +1,102 @@
-interface IInitialization {
-
-}
+interface IInitialization {}
 
 export default class Initialization implements IInitialization {
-  
+  public static IsGlobalMemoryInitialized(): boolean {
+    if (
+      Memory &&
+      Memory.powerCreeps &&
+      Memory.flags &&
+      Memory.rooms &&
+      Memory.spawns &&
+      Memory.structures &&
+      Memory.creeps &&
+      Memory.stats
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  public static InitializeGlobalMemory(): boolean {
+    Memory.flags = {};
+    Memory.rooms = {};
+    Memory.spawns = {};
+    Memory.structures = {};
+    Memory.powerCreeps = {};
+    Memory.creeps = {};
+    Memory.stats = {};
+    return true;
+  }
+
+  public static InitializeRoomMemory(roomName: string): boolean {
+    try {
+      Memory.rooms[roomName] = {};
+      return true;
+    } catch (error) {
+      console.log(error);
+      Game.notify(error);
+      return false;
+    }
+  }
+
+  public static InitializeStructureMemory(id: string): boolean {
+    try {
+      Memory.structures[id] = {};
+      return true;
+    } catch (error) {
+      console.log(error);
+      Game.notify(error);
+      return false;
+    }
+  }
+
+  public static InitializeCreepMemory(id: string): boolean {
+    try {
+      Memory.creeps[id] = {};
+      return true;
+    } catch (error) {
+      console.log(error);
+      Game.notify(error);
+      return false;
+    }
+  }
+
+  public static IsRoomMemoryInitialized(roomName: string): boolean {
+    try {
+      if (Memory.rooms && Memory.rooms[roomName]) {
+        return true;
+      }
+      return false;
+    } catch (error) {
+      console.log(error);
+      Game.notify(error);
+      return false;
+    }
+  }
+
+  public static IsStructureMemoryInitialized(id: string): boolean {
+    try {
+      if (Memory.structures && Memory.structures[id]) {
+        return true;
+      }
+      return false;
+    } catch (error) {
+      console.log(error);
+      Game.notify(error);
+      return false;
+    }
+  }
+
+  public static IsCreepMemoryInitialized(id: string): boolean {
+    try {
+      if (Memory.creeps && Memory.creeps[id]) {
+        return true;
+      }
+      return false;
+    } catch (error) {
+      console.log(error);
+      Game.notify(error);
+      return false;
+    }
+  }
 }
