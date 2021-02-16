b7edddef9c1ff6a2dbf8dc2027da806a003996fb
Pieter <pieterbrandsen2@gmail.com>
2021-02-16 18:33:15 +0100

    Created LogLevel constant/config

--- src/utils/config/global.ts
+++ src/utils/config/global.ts
@@ -1,3 +1,7 @@
+import { LogLevels } from "../constants/global";
+
 interface IGlobalConfig {}
 
-export default class GlobalConfig implements IGlobalConfig {}
+export default class GlobalConfig implements IGlobalConfig {
+  public static LogLevel: LogLevels = LogLevels.All;
+}
--- src/utils/constants/constants.spec.ts
+++ src/utils/constants/constants.spec.ts
@@ -1,11 +1,9 @@
-import GlobalConstants from "./global";
 import RoomConstants from "./room";
 import StructureConstants from "./structure";
 import CreepConstants from "./creep";
 
 describe("Constants", () => {
   it("should have 4 defined classes", () => {
-    expect(GlobalConstants).toBeDefined();
     expect(RoomConstants).toBeDefined();
     expect(StructureConstants).toBeDefined();
     expect(CreepConstants).toBeDefined();
