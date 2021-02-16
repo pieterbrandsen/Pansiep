a4428e64e96946943ef0a224bb400abb568bc9e0
Pieter <pieterbrandsen2@gmail.com>
2021-02-16 11:44:23 +0100

    Added creep, structure and room remove function

--- src/memory/garbageCollection.spec.ts
+++ src/memory/garbageCollection.spec.ts
@@ -1,30 +1,58 @@
-import { mockGlobal, mockInstanceOf } from "screeps-jest";
+import { mockGlobal } from "screeps-jest";
 import GarbageCollection from "./garbageCollection";
 
-const creep = mockInstanceOf<Creep>({});
-const creep2 = mockInstanceOf<Creep>({});
-const creep3 = mockInstanceOf<Creep>({});
-
 describe("GarbageCollection of memory", () => {
-  it("should remove dead creeps", () => {
-    mockGlobal<Game>('Game', {
-      creeps: {
-        creep,
-        creep2,
-        creep3
-      },
+  it("should remove creeps/structures memory on request and cleanup their linked memory", () => {
+    mockGlobal<Memory>("Memory", {
+      creeps: { deadCreep: {} },
+      structures: { dismantledStructure: {} },
     }, true);
-    mockGlobal<Memory>('Memory', { creeps: {"creep": {}, "creep2": {}, "creep3": {}, "deadCreep": {}} });
 
-    GarbageCollection.CheckCreeps();
-    expect(Object.keys(Game.creeps).length).toEqual(3);
+    expect(GarbageCollection.RemoveCreep("deadCreep")).toBeTruthy();
+    expect(
+      GarbageCollection.RemoveStructure("dismantledStructure")
+    ).toBeTruthy();
+    expect(Memory.creeps["deadCreep"]).toBeUndefined();
+    expect(Memory.structures["dismantledStructure"]).toBeUndefined();
+  });
+
+  it("should remove room memory on request and clean up other linked memory", () => {
+    mockGlobal<Memory>(
+      "Memory",
+      {
+        rooms: { unclaimedRoom: {} },
+        creeps: { deadCreep: { spawnRoom: "unclaimedRoom" } },
+        structures: { dismantledStructures: { room: "unclaimedRoom" } },
+      },true
+    );
+    expect(GarbageCollection.RemoveRoom("unclaimedRoom")).toBeTruthy();
+    expect(Memory.rooms["unclaimedRoom"]).toBeUndefined();
+  });
+
+  it("should remove the room but no creeps/structures when there are none", () => {
+    mockGlobal<Memory>(
+      "Memory",
+      {
+        rooms: { unclaimedRoom: {} },
+        creeps: { aliveCreep: { spawnRoom: "claimedRoom" } },
+        structures: {}
+      }, true
+    );
+
+    GarbageCollection.RemoveRoom("unclaimedRoom");
+    expect(Memory.rooms["unclaimedRoom"]).toBeUndefined();
+    expect(Memory.creeps["aliveCreep"]).toBeDefined();
   });
 
   it("should error when Memory.creeps is undefined", () => {
-    mockGlobal<Game>('Game', {notify: jest.fn(() => undefined)});
+    mockGlobal<Game>("Game", { notify: jest.fn(() => undefined) });
     mockGlobal<Game>("console", { log: jest.fn(() => undefined) });
-    mockGlobal<Memory>('Memory', {});
+    mockGlobal<Memory>("Memory", {});
 
-    expect(GarbageCollection.CheckCreeps()).toBeFalsy();
+    expect(GarbageCollection.RemoveCreep("deadCreep")).toBeFalsy();
+    expect(
+      GarbageCollection.RemoveStructure("dismantledStructure")
+    ).toBeFalsy();
+    expect(GarbageCollection.RemoveRoom("unclaimedRoom")).toBeFalsy();
   });
 });
--- src/memory/garbageCollection.ts
+++ src/memory/garbageCollection.ts
@@ -3,14 +3,45 @@ import _ from "lodash";
 interface IGarbageCollection {}
 
 export default class GarbageCollection implements IGarbageCollection {
-  // When do you know when a room/structure should not be saved anymore?
-  public static CheckCreeps():boolean {
+  public static RemoveCreep(name: string): boolean {
     try {
-      for (const name in Memory.creeps) {
-        if (!Game.creeps[name]) {
-          delete Memory.creeps[name];
+      delete Memory.creeps[name];
+      return true;
+    } catch (error) {
+      console.log(error);
+      Game.notify(error);
+      return false;
+    }
+  }
+
+  public static RemoveStructure(id: string): boolean {
+    try {
+      delete Memory.structures[id];
+      return true;
+    } catch (error) {
+      console.log(error);
+      Game.notify(error);
+      return false;
+    }
+  }
+
+  public static RemoveRoom(roomName: string): boolean {
+    try {
+      Object.keys(Memory.creeps).forEach((creepName) => {
+        const creep = Memory.creeps[creepName];
+        if (creep.spawnRoom === roomName) {
+          this.RemoveCreep(creepName);
         }
-      } 
+      });
+
+      Object.keys(Memory.structures).forEach((id) => {
+        const structure = Memory.structures[id];
+        if (structure.room === roomName) {
+          this.RemoveStructure(id);
+        }
+      });
+
+      delete Memory.rooms[roomName];
       return true;
     } catch (error) {
       console.log(error);
