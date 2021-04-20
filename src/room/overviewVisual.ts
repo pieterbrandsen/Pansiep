b7152238a2e127efec6867e596f484eab325b3e6
PieterBrandsen <pieterbrandsen2@gmail.com>
2021-04-20 12:14:55 +0200

    Bug fixes

--- src/room/overviewVisual.ts
+++ src/room/overviewVisual.ts
@@ -8,9 +8,10 @@ import { AddRectWCoords, AddTextWCoords } from "./visuals";
 export const RoomVisuals = FuncWrapper(function RoomVisuals(
   room: Room
 ): FunctionReturn {
-  const textXPos = 1.3;
-  let topLeftPos = 1;
-  AddRectWCoords(room, topLeftPos, topLeftPos, 9, 14, VisualDisplayLevels.Info, {
+  const defaultX = 1;
+  const textXPos = defaultX + 0.3;
+  let topLeftPos = 5;
+  AddRectWCoords(room, defaultX, topLeftPos, 9, 14, VisualDisplayLevels.Info, {
     opacity: 0.65,
     fill: "Grey",
   });
@@ -84,7 +85,7 @@ export const RoomVisuals = FuncWrapper(function RoomVisuals(
 
   // Room
   topLeftPos+=2;
-  AddTextWCoords(room, "Room:", textXPos, 10, VisualDisplayLevels.Info, {
+  AddTextWCoords(room, "Room:", textXPos, topLeftPos, VisualDisplayLevels.Info, {
     align: "left",
     font: 1,
   });
